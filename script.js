10bedeffe9cee7470a49c084c55745bc4ac093f5
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-02-26 14:27:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,35 +8,29 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
+ // const obj = {
+ //    0:['M',1000], 
+ //    1:['D', 500], 
+ //    2:['C', 100], 
+ //    3:['L', 50], 
+ //    4:['X', 10], 
+ //    5:['V', 5], 
+ //    6:['I', 1]
+ //  };
 
- var singles = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-var tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
-var hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-var thousands = ["", "M", "MM", "MMM", "MMMM"];
-//turn the number to a string, split individual characters and then turn each one to a number.
-var length = num.toString().length;
-var numbers = num.toString().split("").map(Number);
-var roman = '';
-var i = 0;
+  let result = "";
+for(let i=0; i<7; i++){
+    let value = obj[i][0];
+    let  romans = obj[i][1];
+    
+    while(nums>=value){
+        result += romans;
+        nums = nums - value
+    }
 
-switch (length) {
-    
-  case 4:
-    roman = thousands[numbers[i]];
-    i++;
-    
-  case 3:
-    roman += hundreds[numbers[i]];
-    i++;
-    
-  case 2:
-    roman += tens[numbers[i]];
-    i++;
-    
-  case 1 :
-    roman += singles[numbers[i]];
 }
-return roman;
+return(result)
+
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
